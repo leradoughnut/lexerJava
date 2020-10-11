d1be58637c782ac34ff522dfda368ff50e0d19f4
Lera Melnychuk <41482994+leradoughnut@users.noreply.github.com>
2020-10-11 12:53:12 +0300

    Hotfix

--- src/lexerJava/Main.java
+++ src/lexerJava/Main.java
@@ -4,18 +4,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
 
     public static void main(String[] args) {
-        String filename = "test.java";
+        System.out.println("Enter the filename:");
+
+        String filename = sc.nextLine();
         Path path = Paths.get(filename);
         File file = path.toFile();
         try{
             Lexer lexer = new Lexer(file);
             System.out.println(lexer.toString());
         }catch (FileNotFoundException e){
-            System.out.println("problem");
+            System.out.println("File not found");
         }
 
     }
